16402b43ffeab6d0f19e97e9d5c6b8708286fa21
hisamura333 <HisamuraTaiki@hisamura-no-MacBook-Air.local>
2016-10-18 11:13:51 +0900

    finish_controller_test

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -6,6 +6,35 @@ describe MessagesController, type: :controller do
       get :index
       expect(response).to render_template :index
     end
+    it "assigns a new message to @message"do
+      get :index
+      expect(assigns(:message)).to be_a Message
+    end
   end
 
+  describe 'POST #create'do
+    let!(:message){build(:message)}
+    let!(:message_params){{body:message.body}}
+
+    it "assigns a create message to @message"do
+      expect do
+        post :create, message: message_params
+      end.to change(Message, :count).by(1)
+    end
+  end
 end
+
+  #     # message = create(:message)
+  #     # post :create, body: message.body
+  #     # expect(assigns(:message)).to eq message
+
+
+
+
+
+
+
+
+
+
+
