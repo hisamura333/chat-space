4d317679e8722976b640332c4a375220ec2679e5
hisamura333 <HisamuraTaiki@hisamura-no-MacBook-Air.local>
2016-10-21 20:32:15 +0900

    Able to update group name

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,6 +7,17 @@ class GroupsController < ApplicationController
     Group.create(create_params)
     redirect_to controller: 'messages',action: 'index'
   end
+
+  def edit
+    @group = Group.find(params[:id])
+    
+  end
+  def update
+    @group = Group.find(params[:id])
+    @group.update(create_params)
+    redirect_to root_path
+    
+  end
   private
   def create_params
     params.require(:group).permit(:name)
