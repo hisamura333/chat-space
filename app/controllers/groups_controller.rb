035da6e6d997e430810708bc41d2cdaf390945a9
hisamura333 <HisamuraTaiki@hisamura-no-MacBook-Air.local>
2016-10-21 12:24:01 +0900

    Save group name

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,14 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
+
+  def create
+    Group.create(create_params)
+    redirect_to controller: 'messages',action: 'index'
+  end
+  private
+  def create_params
+    params.require(:group).permit(:name)
+    
+  end
 end
