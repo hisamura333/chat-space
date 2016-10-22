791e2b9e6ffbe8b6a66b88ed38913a81ebd4d394
hisamura333 <HisamuraTaiki@hisamura-no-MacBook-Air.local>
2016-10-22 14:28:35 +0900

    Add member when group create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -26,7 +26,7 @@ class GroupsController < ApplicationController
   end
   private
   def create_params
-    params.require(:group).permit(:name)
+    params.require(:group).permit(:name,{:user_ids => []})
     
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
   has_many :messages
+  has_many :members
   has_many :users, through: :members
+  accepts_nested_attributes_for :messages
 end
