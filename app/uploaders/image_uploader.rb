f98d724cb00ad4bec3eb1a9275a2da2911ac1b47
hisamura333 <HisamuraTaiki@hisamura-no-MacBook-Air.local>
2016-11-26 11:20:19 +0900

    Apply S3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,6 +8,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
+  #S3 strage
   storage :fog
 
   # Override the directory where uploaded files will be stored.
