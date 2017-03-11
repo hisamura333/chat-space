6c1bbe763807d35f4ef48d8eb72329b3f741f015
hisamura333 <HisamuraTaiki@hisamura-no-MacBook-Air.local>
2017-03-11 16:18:26 +0900

    add flash message when edit group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,12 +21,13 @@ class GroupsController < ApplicationController
     if @group.save
       @group.update(create_params)
       redirect_to group_messages_path(@group.id)
+      flash[:notice] = "変更が完了しました"
     end
   end
 
   private
   def create_params
     params.require(:group).permit(:name,:user_ids => [])
-    
+
   end
 end
